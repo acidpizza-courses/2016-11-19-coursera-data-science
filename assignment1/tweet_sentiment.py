e0be6047641056fe5333f8763f190cacc58fa69d
acidpizza <2451313+acidpizza@users.noreply.github.com>
2016-11-19 00:40:19 +0800

    Done assignment 1 part 1 and 2

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,31 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+scores_ = {} # initialize an empty dictionary
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def LoadDictionary():
+    afinnfile = open("AFINN-111.txt")
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores_[term] = int(score)  # Convert the score to an integer.
+
+def ParseTweet():
+    tweet_file = open(sys.argv[1])
+    for line in tweet_file:
+        score = 0
+        tweet = json.loads(line)
+        if "text" in tweet:
+            tweet_line = tweet["text"]
+            tweet_words = tweet_line.split()
+            for word in tweet_words:
+                if word in scores_:
+                    score += scores_[word]
+        print score
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    LoadDictionary()
+    ParseTweet()
+#    print scores_.items() # Print every (term, score) pair in the dictionary
 
 if __name__ == '__main__':
     main()
